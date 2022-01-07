302daa4319350a0e38874cf52c3d6cb87db5e188
AISK11 <60526069+AISK11@users.noreply.github.com>
2022-01-07 05:59:42 +0100

    Update install.sh

--- configfiles/Network/wpa_supplicant/install/install.sh
+++ configfiles/Network/wpa_supplicant/install/install.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 ## Check if make command was run as root:
 if [[ $(whoami) != "root" ]]; then
@@ -8,3 +8,9 @@ fi
 
 ## Copy local files to their destinations and set file permissions:
 cp ./files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+echo -e "[+]   WPA supplicant configuration file was copied to '/etc/wpa_supplicant/wpa_supplicant.conf'." ||
+echo -e "[-] ! ERROR! WPA supplicant configuration file could not be copied to '/etc/wpa_supplicant/wpa_supplicant.conf'!" ||
+
+chmod 0600 /etc/wpa_supplicant/wpa_supplicant.conf
+echo -e "[+]   WPA supplicant configuration file (/etc/wpa_supplicant/wpa_supplicant.conf) permission secured to 0600." ||
+echo -e "[-] ! ERROR! WPA supplicant configuration file (/etc/wpa_supplicant/wpa_supplicant.conf) permission could not be secured to 0600.! ||
