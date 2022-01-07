9ef22a5a56db7714627b7cca7d11c1af8c9e9123
AISK11 <60526069+AISK11@users.noreply.github.com>
2022-01-07 06:01:20 +0100

    Update install.sh

--- configfiles/Network/wpa_supplicant/install/install.sh
+++ configfiles/Network/wpa_supplicant/install/install.sh
@@ -7,10 +7,10 @@ if [[ $(whoami) != "root" ]]; then
 fi
 
 ## Copy local files to their destinations and set file permissions:
-cp ./files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+cp ./files/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf &&
 echo -e "[+]   WPA supplicant configuration file was copied to '/etc/wpa_supplicant/wpa_supplicant.conf'." ||
-echo -e "[-] ! ERROR! WPA supplicant configuration file could not be copied to '/etc/wpa_supplicant/wpa_supplicant.conf'!" ||
+echo -e "[-] ! ERROR! WPA supplicant configuration file could not be copied to '/etc/wpa_supplicant/wpa_supplicant.conf'!"
 
-chmod 0600 /etc/wpa_supplicant/wpa_supplicant.conf
+chmod 0600 /etc/wpa_supplicant/wpa_supplicant.conf &&
 echo -e "[+]   WPA supplicant configuration file (/etc/wpa_supplicant/wpa_supplicant.conf) permission secured to 0600." ||
-echo -e "[-] ! ERROR! WPA supplicant configuration file (/etc/wpa_supplicant/wpa_supplicant.conf) permission could not be secured to 0600.! ||
+echo -e "[-] ! ERROR! WPA supplicant configuration file (/etc/wpa_supplicant/wpa_supplicant.conf) permission could not be secured to 0600.!"
